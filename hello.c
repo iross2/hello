4f73dfe0fdf17bad878702121e9c426aab838c0f
Isaiah <iross2@huskers.unl.edu>
2023-08-28 19:34:23 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Isaiah Ross
+ * Date: 2023/08/28
+ * Email: iross2@huskers.unl.edu
+ * Hello World Program
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Isaiah Ross\n");
+
+    return 0;
+
+}
